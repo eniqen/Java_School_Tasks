bd1d1f8f53aa486729fceedeafb3a23a63baf661
eniqen <r2n@bk.ru>
2015-05-18 00:05:37 +1000

    Subsequence complete

--- src/com/tsystems/javaschool/tasks/SubsequenceImpl.java
+++ src/com/tsystems/javaschool/tasks/SubsequenceImpl.java
@@ -8,17 +8,17 @@ import java.util.*;
 public class SubsequenceImpl implements Subsequence {
     @Override
     public boolean find(List x, List y) {
-        int count = 0;
-        int marker = 0;
-        for (int i = 0; i < x.size(); i++) {
-            for (int j = marker; j < y.size(); j++) {
-                if (x.get(i).equals(y.get(j))) {
+        int count = 0; //переменная проверяющая совпадает ли кол-во найденых элементов с кол-вом из 1 списка
+        int marker = 0; //маркер от начала которого будем бегать если нашли какой-то эллемент в списке
+        for (int i = 0; i < x.size(); i++) { //луп с первым листом
+            for (int j = marker; j < y.size(); j++) { //с листом в котором проверяем последовательность эллементов
+                if (x.get(i).equals(y.get(j))) { //если эллементы совпадают инкрементим каунт и ставим маркер с данного места
                     count++;
                     marker = j;
                     break;
                 }
             }
         }
-        return x.size() == count ? true : false;
+        return x.size() == count ? true : false; //тернарный оператор возвратит тру если кол-во найденых эллементов совпадет с кол-вом элементов в списке
     }
 }
